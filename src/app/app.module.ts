fc256a149059419220538382a7ea546d0bc3e053
DESKTOP-EB3BLCO\Admin <yeapman@ukr.net>
2017-03-09 16:01:32 +0200

    add 2 buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {routing} from "./app.routing";
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddOrderComponent } from './add-order/add-order.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddOrderComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,8 @@
 import {Routes, RouterModule} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {AddOrderComponent} from "./add-order/add-order.component";
+import {OrderListComponent} from "./order-list/order-list.component";
 
 const appRoutes: Routes = [
   {path: '', component: MainPageComponent},
